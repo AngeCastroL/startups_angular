60070cba3ef32167c8a74a2c1a43ac7da4bf2d73
AngeCastroL <angelicacastro1229@gmail.com>
2024-11-05 16:55:31 -0500

    cambios de css y html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegistroUsuarioComponent } from './componentes/registro-usuario/registro-usuario.component';
-import { RegistroUsuariosComponent } from './componentes/registro-usuarios/registro-usuarios.component';
 import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 export const routes: Routes = [
     {path:'registro-usuario',component:RegistroUsuarioComponent},
-    {path:'registro-usuarios:id',component:RegistroUsuariosComponent},
     {path:'proyecto',component:ProyectoComponent},
     {path:'login',component:LoginComponent},
     {path:'proyectos/:documento',component:ProyectosComponent},
